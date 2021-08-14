ad4509943961ac9e4667613e3a61091779994044
shawcodelife <sonuofficialshaw02@gmail.com>
2021-08-14 20:17:54 +0530

    Promise data fetchig using async and await keywords

--- promises.js
+++ promises.js
@@ -1,15 +1,20 @@
-const obj1 = new Promise( (resolve, rejected)=>{
-    const rollno = [1,2,3,4,5,6,7];
-    setTimeout( ()=>{
-        
-        rollno.length>0 ? resolve(rollno) : rejected('Error in communicating');
-    },2000 );
-} );
-
-
-obj1.then((rollno)=>{
-    rollno.map( (value, index)=>{
-        console.log(value)
+const getPromise = ()=>{
+    return new Promise( (resolve, rejected)=>{
+        const rollno = [1,2,3,4,5,6,7];
+        setTimeout( ()=>{
+            rollno.length>0 ? resolve(rollno) : rejected('Error in communicating');
+        },2000 );
     } );
-}).catch((rejected)=>console.log(rejected));
+}
+
+/* const getData = getPromise();
+getData.then((rollno)=>console.log(rollno)).catch((rejected)=>console.log(rejected)); */
+
+//fetching promise data using async and await, it also avoid then() callback function
+const getData = async ()=>{
+    const getrollno = await getPromise();
+    console.log(getrollno)
+}
+
+getData();
 
